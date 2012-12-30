a52558d6b25885df77a2ba914c48d8cdf35e620c
Geoffrey Wiseman <geoffrey.wiseman@gmail.com>
2012-12-30 01:21:06 -0500

    Fix for spec that relies on hashes being in insertion order, not true in Ruby 1.8.

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -191,130 +191,6 @@ describe ConfigParser do
 
 		end
 
-# target-details is a string (connection), array (array of connection) or hash (target-hash)
-# connection is an integer or string, matching any of: <port> or <localport>:<remoteport> or <localport>:host:<remoteport>
-# target-hash contains: alias, forward, remote-host
-# alias is string or array of strings containing aliases for the tunnel name.
-# forward contains a connection or array of connections
-# remote-host maps to a remote connection or array of remote connections
-# remote connection is an integer or a string in the following formats: <port>, <local-port>:<remote-port>
-
-		# 			context "and no forward" do
-		# 				it { should_not be_valid }
-		# 				specify { subject.errors.should include_match(/no forward/) }
-		# 			end
-
-		# 			context "and a forward" do
-		# 				let(:target) { { 'host' => 'host', 'username' => 'username', 'forward' => forward } }
-		# 				let(:targetmock) { double(Tunnel::Target) }
-
-		# 				before do
-		# 					Tunnel::Target.stub(:new) { targetmock }
-		# 					targetmock.stub( :host ) { 'host' }
-		# 				end
-
-		# 				context "of 8080" do
-		# 					let(:forward) { 8080 }
-		# 					it "should forward port 8080 on localhost" do
-		# 						targetmock.should_receive( :forward_port ).with( 8080, nil )
-		# 						subject.should be_valid
-		# 					end
-		# 				end
-
-		# 				context "of [ 8000, 8443 ]" do
-		# 					let(:forward) { [8000,8443] }
-		# 					it "should forward ports 8080 and 8443" do
-		# 						targetmock.should_receive( :forward_port ).with( 8000, nil )
-		# 						targetmock.should_receive( :forward_port ).with( 8443, nil )
-		# 						subject.should be_valid
-		# 					end
-		# 				end
-
-		# 				context "of { alpha => 3000, beta => [ 8080, 8443 ] } ]" do
-		# 					let(:forward) { { 'alpha' => 3000, 'beta' => [ 8080, 8443 ] } }
-		# 					it "should forward port 3000 to alpha" do
-		# 						targetmock.should_receive( :forward_port ).with( 3000, 'alpha' )
-		# 						targetmock.stub( :forward_port ).with( anything(), 'beta' )
-		# 						subject.should be_valid
-		# 					end
-		# 					it "should forward ports 8080, 8443 to alpha" do
-		# 						targetmock.should_receive( :forward_port ).with( 8080, 'beta' )
-		# 						targetmock.should_receive( :forward_port ).with( 8443, 'beta' )
-		# 						targetmock.stub( :forward_port ).with( anything(), 'alpha' )
-		# 						subject.should be_valid
-		# 					end
-		# 				end
-
-		# 				context "of 8000.5" do
-		# 					let(:forward) { 8000.5 }
-		# 					it { should_not be_valid }
-		# 					specify { subject.errors.should include_match(/Not sure how to handle forward .*: Float/) }
-		# 				end
-
-		# 				context "of -8080" do
-		# 					let(:forward) { -8080 }
-		# 					it { should_not be_valid }
-		# 					specify { subject.errors.should include_match(/Invalid port/) }
-		# 				end
-
-		# 				context "of 'blueberry'" do
-		# 					let(:forward) { 'blueberry' }
-		# 					it { should_not be_valid }
-		# 					specify { subject.errors.should include_match(/Not sure how to handle forward .*: String/) }
-		# 				end
-
-		# 				context "of 'blueberry'" do
-		# 					let(:forward) { [ 80.80, -443, 'blueberry' ] }
-		# 					it { should_not be_valid }
-		# 					specify { subject.errors.should include_match(/Invalid port/) }
-		# 				end
-
-		# 			end
-
-		# 			context "and an alias" do
-		# 				let(:target) { { 'host' => 'host', 'username' => 'username', 'forward' => 8080 } }
-		# 				let(:targetmock) { double(Tunnel::Target) }
-
-		# 				before do
-		# 					Tunnel::Target.stub(:new) { targetmock }
-		# 					targetmock.stub(:forward_port)
-		# 				end
-
-		# 				it "should treat string as single alias" do
-		# 					target['alias']= 'mr-smith'
-		# 					targetmock.should_receive( :alias ).with( 'mr-smith' )
-		# 					subject.should be_valid
-		# 				end
-
-		# 				it "should treat an array as a series of aliases" do
-		# 					target['alias']= 'mr-smith'
-		# 					targetmock.should_receive( :alias ).with( 'mr-smith' )
-		# 					subject.should be_valid
-		# 				end
-
-		# 				it "should warn with any other content" do
-		# 					target['alias'] = Hash.new
-		# 					subject.should_not be_valid
-		# 					subject.errors.should include_match( /Cannot parse alias/ )
-		# 				end
-		# 			end
-		# 		end
-
-		# 	end
-
-		# 	context "containing a hash of five targets" do
-		# 		let(:config) { { 'alpha' => { 'host' => 'host', 'forward' => 3001 }, 'beta' => { 'host' => 'host', 'forward' => 3002 }, 'gamma' => { 'host' => 'host', 'forward' => 3003 }, 'delta' => { 'host' => 'host', 'forward' => 3004 },
-		# 			'omega' => { 'host' => 'host', 'forward' => 3005 } } }
-		# 		it "should return all five targets specified" do
-		# 			subject.should be_valid
-		# 			subject.get_target( 'alpha' ).should be_instance_of(Tunnel::Target)
-		# 			subject.get_target( 'beta' ).should be_instance_of(Tunnel::Target)
-		# 			subject.get_target( 'gamma' ).should be_instance_of(Tunnel::Target)
-		# 			subject.get_target( 'delta' ).should be_instance_of(Tunnel::Target)
-		# 			subject.get_target( 'omega' ).should be_instance_of(Tunnel::Target)
-		# 		end
-		# 	end
-
 	end
 end
 
@@ -336,9 +212,10 @@ end
 RSpec::Matchers.define :have_tunnels do |expected|
 	match do |actual|
 		actual.should_not be_nil
-		actual.tunnels.size.should eql(expected.size) 
+		tunnels = actual.tunnels.sort_by { |t| t.port }
+		tunnels.size.should eql(expected.size)
 		(0...expected.size).each do |index|
-			tunnel = actual.tunnels[index]
+			tunnel = tunnels[index]
 			properties = expected[index]
 			properties.each do |k,v|
 				tunnel.send(k).should eql(v)
