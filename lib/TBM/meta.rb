eb3f9463799c3ea69aa41b1e02a5ce14593be691
Geoffrey Wiseman <geoffrey.wiseman@codiform.com>
2017-12-14 09:59:17 -0500

    Working on v0.5
    Signed-off-by: Geoffrey Wiseman <geoffrey.wiseman@codiform.com>

--- lib/TBM/meta.rb
+++ lib/TBM/meta.rb
@@ -1,6 +1,6 @@
 # The namespace for this application/gem.
 module TBM
   APP_NAME = "Tunnel Boring Machine"
-  VERSION = "0.4.0"
-  RELEASE_DATE = '2017-12-14'
+  VERSION = "0.5.0"
+  RELEASE_DATE = '2017-12-31'
 end
